fe35b015c1e9cb4d581a01f8e52f93959686cab1
akramuim <o.akramuim@gmail.com>
2021-05-22 09:25:57 +0100

    Add download root

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+
+# Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -25,8 +28,25 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User(
+            name=name,
+            email=email,
+            password=hash_and_salted_password,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return render_template("secrets.html", name=user.name)
     return render_template("register.html")
 
 
@@ -45,9 +65,9 @@ def logout():
     pass
 
 
-@app.route('/download')
+@app.route('/download', methods=['GET'])
 def download():
-    pass
+    return send_from_directory('static', filename="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
